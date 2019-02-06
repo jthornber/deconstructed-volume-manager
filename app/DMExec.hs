4018f7982a216fe33311370b41d2e49d52d1b73a
Joe Thornber <ejt@redhat.com>
2019-02-06 12:15:50 +0000

    [dmexec] check loading a table with more than one target works

--- app/DMExec.hs
+++ app/DMExec.hs
@@ -15,12 +15,14 @@ import qualified Data.Text as T
 -- I think we need our own DMExec monad so we can
 -- handle errors nicely
 
+errorTarget len = TableLine (T.pack "error") len (T.pack "")
+
 dmExecCmd :: [Text] -> IO ()
 dmExecCmd _ =
     withControlDevice $ \ctrl -> do
         removeAll ctrl
         createDevice name uuid ctrl
-        loadTable name uuid [TableLine (T.pack "error") 1024 (T.pack "")] ctrl
+        loadTable name uuid [errorTarget 1024, errorTarget 4096] ctrl
         r <- listDevices ctrl
         putStrLn . show $ r
         suspendDevice name uuid ctrl
